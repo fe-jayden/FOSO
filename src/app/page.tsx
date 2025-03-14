62d1227406acc4c80bf4584bddca2f9cd827d26b
fe-jayden <jayden.fe.dev@gmail.com>
2025-03-14 16:19:45 +0700

    fix and push code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="container text-center relative">
       <main className="container py-8 ">
-
+        <h1>Page Home</h1>
       </main>
     </div>
   );
