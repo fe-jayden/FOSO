300c4b97219810976439c75a6f2a48877fd73923
fe-jayden <jayden.fe.dev@gmail.com>
2025-03-14 17:46:28 +0700

    fix: err

--- src/app/introduce/[introSub]/page.tsx
+++ src/app/introduce/[introSub]/page.tsx
@@ -1,17 +1,8 @@
-import { FC } from "react";
-
-interface IIntroSubPageProps {
-  params: { introSub: string };
-}
-
-const IntroSubPage: FC<IIntroSubPageProps> = ({ params }) => {
-  console.log("Params received:", params);
+export default async function IntroSubPage({ params }: { params: { introSub: string } }) {
   return (
     <div>
-      <h1>Giới thiệu về {params?.introSub}</h1>
-      <p>Trang này hiển thị thông tin về {params?.introSub}.</p>
+      <h1>Giới thiệu về {params.introSub}</h1>
+      <p>Trang này hiển thị thông tin về {params.introSub}.</p>
     </div>
   );
-};
-
-export default IntroSubPage;
+}
